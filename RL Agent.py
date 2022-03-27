4a13e282d9a2b6b13980d45a85279824a5c32638
Jasper <jasperhavenhand@gmail.com>
2022-03-27 13:44:54 +0100

    Adding the RL Agent file.

---
+++ RL Agent.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 27 12:41:07 2022
+
+@author: Jasper Havenhand
+"""
+# import gym
+# env = gym.make('Alien-v0', render_mode='human')
+# env.reset()
+# for _ in range(1000):
+#     env.step(env.action_space.sample()) # take a random action
+# env.close()
+
+import gym
+env = gym.make('Alien-v0', render_mode='human')
+for i_episode in range(20):
+    observation = env.reset()
+    t = 0
+    while True:
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+        t += 1
+env.close()
